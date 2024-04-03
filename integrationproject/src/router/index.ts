6c03e4b1c87f80439f196a87eaaf93587307586b
magarmanoj <manoj.magar@student.odisee.be>
2024-04-03 14:18:29 +0200

    index paginas

--- integrationproject/src/router/index.ts
+++ integrationproject/src/router/index.ts
@@ -1,17 +1,34 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TestPage from '@/views/TestPage.vue';
+import TabsPage from '@/views/TabsPage.vue'
 
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/test'
+    redirect: '/tabs/tabHome'
   },
   {
-    path: '/test', 
-    name: 'Test',
-    component: TestPage 
+    path: '/tabs/',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/tabHome'
+      },
+      {
+        path: 'tabHome',
+        component: () => import('@/views/HomePage.vue')
+      },
+      {
+        path: 'tabInfo',
+        component: () => import('@/views/InfoPage.vue')
+      },
+      {
+        path: 'tabTest',
+        component: () => import('@/views/TestPage.vue')
+      }
+    ]
   }
 ]
 
--- integrationproject/src/views/HomePage.vue
+++ integrationproject/src/views/HomePage.vue
@@ -1,56 +1,49 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
+      <ion-header>
+          <ion-toolbar>
+              <ion-title class="ion-text-center">Home</ion-title>
+          </ion-toolbar>
       </ion-header>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
+      <ion-content class="homeContent">
+          <ion-grid class="homeGrid">
+              <ion-row>
+                  <ion-col class="ion-text-center">
+                      <ion-button class="homeBtn" expand="full" href="/tabs/tabHome">
+                          <ion-icon class="homeIcon" :icon="desktop"></ion-icon>
+                      </ion-button>
+                  </ion-col>
+                  <ion-col class="ion-text-center">
+                      <ion-button class="homeBtn" expand="full" href="/tabs/tabTest">
+                          <ion-icon class="homeIcon" :icon="listOutline"></ion-icon>
+                      </ion-button>
+                  </ion-col>
+                  <ion-col class="ion-text-center">
+                      <ion-button class="homeBtn" expand="full" href="/tabs/tabInfo">
+                          <ion-icon class="homeIcon" :icon="informationCircle"></ion-icon>
+                      </ion-button>
+                  </ion-col>
+              </ion-row>
+          </ion-grid>
+      </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {home, information, listOutline } from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'TabsPage',
+  data() {
+      return {
+        desktop: home,
+        listOutline: listOutline,
+        informationCircle: information
+      };
+    }
+})
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
