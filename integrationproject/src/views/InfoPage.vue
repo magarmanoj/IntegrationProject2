ed0ffff30920d25cb152e26635ab9697858342e0
magarmanoj <manoj.magar@student.odisee.be>
2024-04-03 23:39:21 +0200

    infopage

--- integrationproject/src/views/InfoPage.vue
+++ integrationproject/src/views/InfoPage.vue
@@ -7,104 +7,94 @@
     </ion-header>
 
     <ion-content class="ion-padding">
-      <div class="content-wrapper">
-        <div class="about-container">
-          <section id="about-odisee" class="about-section">
-            <img src="/img/Odisee_Logo_Co-hogeschool.png" alt="Odisee Logo" class="about-logo">
-            <h2>Over Ons</h2>
-            <p>
-              Wij zijn zes tweedejaars informatica studenten van Odisee Hogeschool: [namen van de studenten].
-              Voor ons project 'Integration Project 2' werken we samen met Axxes, een bekend IT-consultancybedrijf.
-              Deze samenwerking geeft ons de kans om onze kennis in praktijk te brengen en mee te werken aan echte
-              IT-oplossingen.
-            </p>
-          </section>
-          <section id="about-axxes" class="about-section">
-            <img src="/img/og-image.jpg" alt="Axxes Logo" class="about-logo">
-            <h3>Onze Klant</h3>
-            <p>
-              We ontwikkelen voor Axxes een slimme tool die laat zien hoe druk het op kantoor zal zijn. Deze tool
-              gebruikt informatie zoals geplande vergaderingen om te voorspellen wanneer het kantoor vol of juist
-              rustig is. Zo kan iedereen makkelijk beslissen wanneer het beste is om naar kantoor te komen.
-              We zorgen ervoor dat deze tool veilig en makkelijk in gebruik is, en goed werkt op computers, mobiele
-              telefoons en tablets. We proberen de kosten laag te houden en de gebruiksvriendelijkheid hoog.
-            </p>
-          </section>
-        </div>
-      </div>
+      <ion-grid>
+        <ion-row>
+          <ion-col size-md="6">
+            <ion-card class="info-card">
+              <div class="card-content">
+                <img src="/img/Odisee_Logo_Co-hogeschool.png" alt="Odisee Logo" class="about-logo">
+                <ion-card-header>
+                  <ion-card-title>Over Ons</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  Wij zijn zes tweedejaars informatica studenten van Odisee Hogeschool: [namen van de studenten]. Voor
+                  ons
+                  project 'Integration Project 2' werken we samen met Axxes, een bekend IT-consultancybedrijf. Deze
+                  samenwerking geeft ons de kans om onze kennis in praktijk te brengen en mee te werken aan echte
+                  IT-oplossingen.
+                </ion-card-content>
+              </div>
+            </ion-card>
+          </ion-col>
+          <ion-col size-md="6">
+            <ion-card class="info-card">
+              <div class="card-content">
+                <img src="/img/og-image.jpg" alt="Axxes Logo" class="about-logo">
+                <ion-card-header>
+                  <ion-card-title>Onze Klant</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  We ontwikkelen voor Axxes een slimme tool die laat zien hoe druk het op kantoor zal zijn. Deze tool
+                  gebruikt informatie zoals geplande vergaderingen om te voorspellen wanneer het kantoor vol of juist
+                  rustig is. Zo kan iedereen makkelijk beslissen wanneer het beste is om naar kantoor te komen. We
+                  zorgen
+                  ervoor dat deze tool veilig en makkelijk in gebruik is, en goed werkt op computers, mobiele telefoons
+                  en
+                  tablets. We proberen de kosten laag te houden en de gebruiksvriendelijkheid hoog.
+                </ion-card-content>
+              </div>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
-  
-  <script lang="ts">
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFooter } from '@ionic/vue';
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    components: {
-      IonPage,
-      IonHeader,
-      IonToolbar,
-      IonTitle,
-      IonContent,
-      IonFooter
-    },
-    name: 'HomePage'
-  });
-  </script>
-  
-  <style scoped>
-  .content-wrapper {
-    display: flex;
-    justify-content: center;
-  }
 
-  .about-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    max-width: 1200px; 
-  }
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonCard, IonGrid, IonCol, IonRow, IonContent, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/vue';
+import { defineComponent } from 'vue';
 
-  .about-section {
-    flex-basis: calc(50% - 20px); 
-    margin-bottom: 20px;
-    text-align: center;
-  }
+export default defineComponent({
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonCard,
+    IonGrid,
+    IonCol,
+    IonRow
+  },
+  name: 'HomePage'
+});
+</script>
 
-  .about-section p {
-    text-align: left; 
-    margin: 0 auto; 
-  }
+<style scoped>
+.about-logo {
+  max-width: 75%;
+  height: auto;
+  margin: 0 auto;
+}
 
-  .about-logo {
-    width: 250px; 
-    height: auto; 
-    margin-bottom: 10px;
-  }
+.info-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 100%;
+  text-align: center;
+}
 
-  h2,
-  h3 {
-    font-size: 1.5em;
-    margin-bottom: 10px;
-  }
-
-  p {
-    font-size: 1em;
-    line-height: 1.5;
-  }
-
-  ion-footer {
-    text-align: center;
-    margin-top: 20px;
-  }
-
-  /* Media Queries */
-  @media only screen and (max-width: 800px) {
-    .about-section {
-      flex-basis: 100%;
-    }
-  } 
-  </style>
-  
+.card-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
