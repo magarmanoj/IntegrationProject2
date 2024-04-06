ce1fe63793b38b10de1484918b49a7469d1fcbc7
prashantMAX <118283008+prashantMAX@users.noreply.github.com>
2024-04-06 13:20:02 +0200

    Info page Design Update

--- integrationproject/src/views/InfoPage.vue
+++ integrationproject/src/views/InfoPage.vue
@@ -9,27 +9,41 @@
     <ion-content class="ion-padding">
       <div class="content-wrapper">
         <div class="about-container">
-          <section id="about-odisee" class="about-section">
-            <img src="/img/Odisee_Logo_Co-hogeschool.png" alt="Odisee Logo" class="about-logo">
-            <h2>Over Ons</h2>
-            <p>
-              Wij zijn zes tweedejaars informatica studenten van Odisee Hogeschool: [namen van de studenten].
-              Voor ons project 'Integration Project 2' werken we samen met Axxes, een bekend IT-consultancybedrijf.
-              Deze samenwerking geeft ons de kans om onze kennis in praktijk te brengen en mee te werken aan echte
-              IT-oplossingen.
-            </p>
-          </section>
-          <section id="about-axxes" class="about-section">
-            <img src="/img/og-image.jpg" alt="Axxes Logo" class="about-logo">
-            <h3>Onze Klant</h3>
-            <p>
-              We ontwikkelen voor Axxes een slimme tool die laat zien hoe druk het op kantoor zal zijn. Deze tool
-              gebruikt informatie zoals geplande vergaderingen om te voorspellen wanneer het kantoor vol of juist
-              rustig is. Zo kan iedereen makkelijk beslissen wanneer het beste is om naar kantoor te komen.
-              We zorgen ervoor dat deze tool veilig en makkelijk in gebruik is, en goed werkt op computers, mobiele
-              telefoons en tablets. We proberen de kosten laag te houden en de gebruiksvriendelijkheid hoog.
-            </p>
-          </section>
+          <table class="info-table">
+            <tr>
+              <th colspan="2">Over Ons</th>
+            </tr>
+            <tr>
+              <td>
+                <img src="/img/Odisee_Logo_Co-hogeschool.png" alt="Odisee Logo" class="about-logo">
+              </td>
+              <td>
+                <p>
+                  Wij zijn zes tweedejaars informatica studenten van Odisee Hogeschool: [namen van de studenten].
+                  Voor ons project 'Integration Project 2' werken we samen met Axxes, een bekend IT-consultancybedrijf.
+                  Deze samenwerking geeft ons de kans om onze kennis in praktijk te brengen en mee te werken aan echte
+                  IT-oplossingen.
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <th colspan="2">Onze Klant</th>
+            </tr>
+            <tr>
+              <td>
+                <img src="/img/og-image.jpg" alt="Axxes Logo" class="about-logo">
+              </td>
+              <td>
+                <p>
+                  We ontwikkelen voor Axxes een slimme tool die laat zien hoe druk het op kantoor zal zijn. Deze tool
+                  gebruikt informatie zoals geplande vergaderingen om te voorspellen wanneer het kantoor vol of juist
+                  rustig is. Zo kan iedereen makkelijk beslissen wanneer het beste is om naar kantoor te komen.
+                  We zorgen ervoor dat deze tool veilig en makkelijk in gebruik is, en goed werkt op computers, mobiele
+                  telefoons en tablets. We proberen de kosten laag te houden en de gebruiksvriendelijkheid hoog.
+                </p>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </ion-content>
@@ -42,75 +56,58 @@
   </ion-page>
 </template>
 
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFooter } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonFooter
+  },
+  name: 'HomePage'
+});
+</script>
+
+<style scoped>
+.content-wrapper {
+  display: flex;
+  justify-content: center;
+  background-color: #f7efef;
+}
+
+.about-container {
+  max-width: 1200px;
+}
+
+.about-logo {
   
-  <script lang="ts">
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFooter } from '@ionic/vue';
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    components: {
-      IonPage,
-      IonHeader,
-      IonToolbar,
-      IonTitle,
-      IonContent,
-      IonFooter
-    },
-    name: 'HomePage'
-  });
-  </script>
-  
-  <style scoped>
-  .content-wrapper {
-    display: flex;
-    justify-content: center;
-  }
+  height: auto;
+}
 
-  .about-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    max-width: 1200px; 
-  }
+.info-table {
+  width: 100%;
+  border-collapse: collapse;
+}
 
-  .about-section {
-    flex-basis: calc(50% - 20px); 
-    margin-bottom: 20px;
-    text-align: center;
-  }
+.info-table th {
+  text-align: center;
+  font-size: 1.5em;
+}
 
-  .about-section p {
-    text-align: left; 
-    margin: 0 auto; 
-  }
+.info-table td {
+  vertical-align: top;
+  padding: 20px;
+  text-align: left;
+  font-size: 1em;
+}
 
-  .about-logo {
-    width: 250px; 
-    height: auto; 
-    margin-bottom: 10px;
-  }
-
-  h2,
-  h3 {
-    font-size: 1.5em;
-    margin-bottom: 10px;
-  }
-
-  p {
-    font-size: 1em;
-    line-height: 1.5;
-  }
-
-  ion-footer {
-    text-align: center;
-    margin-top: 20px;
-  }
-
-  /* Media Queries */
-  @media only screen and (max-width: 800px) {
-    .about-section {
-      flex-basis: 100%;
-    }
-  } 
-  </style>
-  
+ion-footer {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
