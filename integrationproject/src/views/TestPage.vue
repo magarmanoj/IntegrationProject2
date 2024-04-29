7182ed7dc0afc486cd4ad2e4e4282269b46108a6
magarmanoj <manoj.magar@student.odisee.be>
2024-04-29 23:47:49 +0200

    Testing

--- integrationproject/src/views/TestPage.vue
+++ integrationproject/src/views/TestPage.vue
@@ -12,15 +12,15 @@
           <ion-select-option value="total_number">Total Number</ion-select-option>
         </ion-select>
       </ion-item>
-      <ion-card v-for="(item, index) in filterData" :key="index">
-        <ion-card-header>
-          <ion-card-title>{{ item.Locatie }}</ion-card-title>
-        </ion-card-header>
-        <ion-card-content>
-          <ion-label>Datum: {{ item.Datum }}</ion-label><br>
-          <ion-label>Number of Logins: {{ item.TotalAantalLogin }}</ion-label>
-        </ion-card-content>
-      </ion-card>
+      <ion-card v-for="(item, index) in displayedData" :key="index">
+    <ion-card-header>
+      <ion-card-title>{{ item.Locatie }}</ion-card-title>
+    </ion-card-header>
+    <ion-card-content>
+      <ion-label>Datum: {{ item.TimeSlot }}</ion-label><br>
+      <ion-label>Number of Logins: {{ item.TotalAantalLogin }}</ion-label>
+    </ion-card-content>
+  </ion-card>
     </ion-content>
   </ion-page>
 </template>
@@ -31,7 +31,7 @@ import axios from 'axios';
 import { ref, onMounted, computed } from 'vue';
 
 interface DataItem {
-  Datum: string;
+  TimeSlot: string;
   Locatie: string;
   AantalLogin: string;
 }
@@ -39,80 +39,54 @@ interface DataItem {
 const data = ref<DataItem[]>([]);
 const selectedFilter = ref<string>("alphabetical"); 
 
-const getDetails = () => {
-  axios.post('https://www.gauravghimire.be/API_DrukBarometer/GetDetails.php')
-    .then(response => {
-      if (response.status !== 200) {
-        console.log(response.status);
-        return;
-      }
-
-      if (!response.data.data) {
-        console.log('response.data.data is not ok');
-        return;
-      }
-
-      data.value = response.data.data;
-    })
+const getDetails = (locatie: string) => {
+  const postData = {
+    locatie: locatie 
+  };
+  axios.post('https://www.gauravghimire.be/API_DrukBarometer/datePerLocation.php', postData)
+  .then(response => {
+  console.log(response.data); // Log to check the data
+  if (response.status === 200 && response.data.data) {
+    data.value = response.data.data;
+    console.log(data.value); // Log to confirm that data is set
+  } else {
+    console.error('Response not OK:', response);
+  }
+})
     .catch(error => {
       console.error('Error fetching data:', error);
     });
 };
 
-const getFilteredData = () => {
-  axios.post('https://manojmagar.be/RESTfulAPI/Taak1/api/Medewerkerget.php')
-    .then(response => {
-      if (response.status !== 200) {
-        console.log(response.status);
-      }
-      if (!response.data.data) {
-        console.log('response.data.data is not ok');
-        return;
-      }
-      selectedFilter.value = response.data.data.map((selectedFilter: any) => ({
-        ...selectedFilter
-      }));
-    })
-    .catch(error => {
-      console.error('Error fetching Medewerkers:', error);
-    });
-};
-
 onMounted(() => {
-  getDetails();
-  getFilteredData();
+  getDetails("airtame");
 });
 
 const displayedData = computed(() => {
-  const aggregatedMap = new Map<string, { Datum: string; TotalAantalLogin: number }>();
+  const aggregatedMap = new Map<string, { TimeSlot: string; TotalAantalLogin: number }>();
   data.value.forEach((item) => {
-    const locatie = item.Locatie;
-    const aantalLogin = Number(item.AantalLogin);
-    const datum = item.Datum;
-    if (!isNaN(aantalLogin)) { 
-      if (aggregatedMap.has(locatie)) {
-        const currentValue = aggregatedMap.get(locatie)!;
-        aggregatedMap.set(locatie, { Datum: datum, TotalAantalLogin: currentValue.TotalAantalLogin + aantalLogin });
+    // Assuming item.AantalLogin is a string from the API that can be converted to a number
+    const aantalLogin = parseInt(item.AantalLogin);
+    if (!isNaN(aantalLogin)) {
+      if (aggregatedMap.has(item.Locatie)) {
+        const existing = aggregatedMap.get(item.Locatie)!;
+        existing.TotalAantalLogin += aantalLogin;
+        // Compare and keep the most recent TimeSlot
+        if (item.TimeSlot > existing.TimeSlot) {
+          existing.TimeSlot = item.TimeSlot;
+        }
       } else {
-        aggregatedMap.set(locatie, { Datum: datum, TotalAantalLogin: aantalLogin });
+        aggregatedMap.set(item.Locatie, {
+          TimeSlot: item.TimeSlot,
+          TotalAantalLogin: aantalLogin
+        });
       }
     }
   });
-  return Array.from(aggregatedMap).map(([Locatie, { Datum, TotalAantalLogin }]) => ({
+  return Array.from(aggregatedMap, ([Locatie, content]) => ({
     Locatie,
-    Datum,
-    TotalAantalLogin
+    ...content
   }));
 });
 
-const filterData = computed(() => {
-  const filteredData = [...displayedData.value];
-  if (selectedFilter.value == "alphabetical") {
-    return filteredData.sort((a, b) => a.Locatie.localeCompare(b.Locatie));
-  } else if (selectedFilter.value == "total_number") {
-    return filteredData.sort((a, b) => b.TotalAantalLogin - a.TotalAantalLogin);
-  } else {
-    return filteredData; 
-  }
-});
 </script>
